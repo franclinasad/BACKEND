d677e0145669310f7de7058c38d6080bbe70d9e3
John Limber Illimani Choqueta <illimanijohn09@gmail.com>
2024-05-23 20:37:03 -0400

    version 1.0 del backend JOHN LIMBER ILLIMANI CHOQUETA

---
+++ src/main/java/com/gestion/pagos/GestionPagosApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPagosApplication.class, args);
+	}
+
+}
